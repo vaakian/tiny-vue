5af17830402bad361bfe63d8e5795157c33bba50
vaakian <vaakian@gmail.com>
2022-04-17 15:47:38 +0800

    type definition fixes.

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -13,17 +13,25 @@ export type Key = string | symbol
 // cache the reactive result of each `target`
 type ProxyCache = WeakMap<any, any>
 
-type DeepReadonly<T> = {
-  readonly [P in keyof T]: T[P] extends {} ? DeepReadonly<T[P]> : T[P]
-}
-
 export const enum ReactiveFlags {
-  IS_REACTIVE = '__v_isReactive',
   IS_READONLY = '__v_isReadonly',
+  IS_REACTIVE = '__v_isReactive',
   RAW = '__v_raw',
 }
+type InternalFlag<T> = {
+  [ReactiveFlags.IS_READONLY]: boolean,
+  [ReactiveFlags.IS_REACTIVE]: boolean,
+  [ReactiveFlags.RAW]: T
+}
 
+type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends Record<any, any> ? DeepReadonly<T[P]> : T[P]
+} & InternalFlag<T>
 
+type Reactive<T> = T & InternalFlag<T>
+type DeepReactive<T> = {
+  [P in keyof T]: T[P] extends Record<any, any> ? DeepReactive<T[P]> : T[P]
+} & InternalFlag<T>
 // to cache the reactive result of each `target`
 // by using WeakMap, the `value` can be automatically garbage collected
 // when `target` is no more strongly referenced
@@ -33,7 +41,7 @@ const readonlyMap: ProxyCache = new WeakMap()
 const shallowReactiveMap: ProxyCache = new WeakMap()
 const shallowReadonlyMap: ProxyCache = new WeakMap()
 
-function createReactiveObject<T extends Record<Key, any>>(target: T, proxyMap: ProxyCache, baseHandler: ProxyHandler<T>): T {
+function createReactiveObject<T extends Record<Key, any>>(target: T, proxyMap: ProxyCache, baseHandler: ProxyHandler<T>): Reactive<T> {
   // create a target => depsMap(stores `effects` for each property)
   // duplicated proxy is cached, safe to use
   if (!proxyMap.get(target)) {
@@ -44,17 +52,20 @@ function createReactiveObject<T extends Record<Key, any>>(target: T, proxyMap: P
   return proxyMap.get(target)
 }
 
-export function reactive<T extends Record<Key, any>>(target: T): T {
-  return createReactiveObject(target, reactiveMap, mutableHandlers) as T
+export function reactive<T extends Record<Key, any>>(target: T) {
+  return createReactiveObject(target, reactiveMap, mutableHandlers) as DeepReactive<T>
 }
 
 export function readonly<T extends Record<Key, any>>(target: T): DeepReadonly<T> {
   return createReactiveObject(target, readonlyMap, readonlyHandlers) as DeepReadonly<T>
 }
 
-export function shallowReactive<T extends Record<Key, any>>(target: T): T {
-  return createReactiveObject(target, shallowReactiveMap, shallowReactiveHandlers) as T
+export function shallowReactive<T extends Record<Key, any>>(target: T) {
+  return createReactiveObject(target, shallowReactiveMap, shallowReactiveHandlers)
 }
 export function shallowReadonly<T extends Record<Key, any>>(target: T): Readonly<T> {
-  return createReactiveObject(target, shallowReadonlyMap, shallowReadonlyHandlers) as Readonly<T>
+  return createReactiveObject(target, shallowReadonlyMap, shallowReadonlyHandlers) as Readonly<T> & InternalFlag<T>
+}
+export function toRaw<T>(target: Reactive<T>): T {
+  return target[ReactiveFlags.RAW] || target
 }
